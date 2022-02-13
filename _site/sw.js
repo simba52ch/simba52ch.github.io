4072ecb35a185f3f69b31a4f0a63467f273f4f86
simba52ch <yoojin5589@unist.ac.kr>
2022-02-13 08:36:45 +0000

    v1.0

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220213.1632';
+const cacheName = 'chirpy-20220213.1636';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
